d7caa868cc819dcb0ac22d3f7382658147620422
yunizzang <wldbszp@gmail.com>
2023-02-03 13:23:21 +0900

    Feat: firebase 환경변수로 변경

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,18 @@
  import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+console.log(
+  process.env.REACT_APP_FIREBASE_API_KEY,
+  process.env.REACT_APP_AUTH_DOMAIN
+)
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "chat-b329a.firebaseapp.com",
-  projectId: "chat-b329a",
-  storageBucket: "chat-b329a.appspot.com",
-  messagingSenderId: "21115487653",
-  appId: "1:21115487653:web:cff45a9465192a0e0ed44c",
-  measurementId: "G-SWG9B5YG3K"
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
