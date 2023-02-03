9145c7589848010eafc064eb57c7f00571272f8f
yunizzang <wldbszp@gmail.com>
2023-02-03 10:47:57 +0900

    Env: firebase 패키지 추가 및 설정

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+ import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCoywehXUKEN7jSaCUPINsOAMZfOirxV-A",
+  authDomain: "chat-b329a.firebaseapp.com",
+  projectId: "chat-b329a",
+  storageBucket: "chat-b329a.appspot.com",
+  messagingSenderId: "21115487653",
+  appId: "1:21115487653:web:cff45a9465192a0e0ed44c",
+  measurementId: "G-SWG9B5YG3K"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
